6b65a5d9b3c6ff0dad2aa3091b66d4d3b0f5abc8
Rattaphon Hokking <ratthapon.h@gmail.com>
2020-03-19 17:30:17 +0700

    First commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='vggvox',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+
+    # defined in MANIFEST.in
+    include_package_data=True
+)
---
+++ vggvox/constants.py
@@ -0,0 +1,16 @@
+import pkg_resources
+
+# Signal processing
+SAMPLE_RATE = 16000
+PREEMPHASIS_ALPHA = 0.97
+FRAME_LEN = 0.025
+FRAME_STEP = 0.01
+NUM_FFT = 512
+BUCKET_STEP = 1
+MAX_SEC = 10
+
+# Model
+WEIGHTS_FILE = pkg_resources.resource_filename('data', 'model/weights.h5')
+COST_METRIC = "cosine"  # euclidean or cosine
+INPUT_SHAPE=(NUM_FFT,None,1)
+
