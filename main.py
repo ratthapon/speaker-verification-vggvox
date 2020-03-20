0753848762488af9bf652bfb19171dadcbb0a1b6
Rattaphon Hokking <ratthapon.h@gmail.com>
2020-03-20 14:57:33 +0700

    Refactored VGGVox model

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import numpy as np
+import scipy.io.wavfile as wav
 
 from vggvox import constants as config
 import vggvox.model
-from vggvox import feature_extraction
+from vggvox.feature_extraction import FastFourierTransform
 
 
 model = vggvox.model.vggvox_model()
@@ -12,35 +13,16 @@ model.summary()
 
 def test_verification():
     print("Processing enroll samples....")
-    buckets = feature_extraction.build_buckets()
-    feature = feature_extraction.get_fft_spectrum('data/wav/enroll/19-227-0000.wav', buckets)
+    # buckets = FastFourierTransform.build_buckets()
+    # signal = FastFourierTransform.load_wav('data/wav/enroll/19-227-0000.wav')
+    (fs, signal) = wav.read('data/wav/enroll/19-227-0000.wav')
+    print(signal.shape, signal[1:100])
+    feature = FastFourierTransform.get_fft_spectrum(signal)
+    print(feature[1:100])
     print(feature.shape)
 
     result = model.predict(feature.reshape(1, *feature.shape, 1))
     embeded = np.squeeze(result)
-
-# enroll_result = get_embeddings_from_list_file(model, c.ENROLL_LIST_FILE, c.MAX_SEC)
-# enroll_embs = np.array([emb.tolist() for emb in enroll_result['embedding']])
-# speakers = enroll_result['speaker']
-
-# print("Processing test samples....")
-# test_result = get_embeddings_from_list_file(model, c.TEST_LIST_FILE, c.MAX_SEC)
-# test_embs = np.array([emb.tolist() for emb in test_result['embedding']])
-#
-# print("Comparing test samples against enroll samples....")
-# distances = pd.DataFrame(cdist(test_embs, enroll_embs, metric=c.COST_METRIC), columns=speakers)
-#
-# scores = pd.read_csv(c.TEST_LIST_FILE, delimiter=",",header=0,names=['test_file','test_speaker'])
-# scores = pd.concat([scores, distances],axis=1)
-# scores['result'] = scores[speakers].idxmin(axis=1)
-# scores['correct'] = (scores['result'] == scores['test_speaker'])*1. # bool to int
-#
-# print("Writing outputs to [{}]....".format(c.RESULT_FILE))
-# result_dir = os.path.dirname(c.RESULT_FILE)
-# if not os.path.exists(result_dir):
-#     os.makedirs(result_dir)
-# with open(c.RESULT_FILE, 'w') as f:
-# 	scores.to_csv(f, index=False)
 
 
 if __name__ == '__main__':
--- vggvox/feature_extraction.py
+++ vggvox/feature_extraction.py
@@ -1,72 +1,77 @@
-import librosa
 import numpy as np
 from scipy.signal import lfilter, butter
-
 from python_speech_features import sigproc
+
 import vggvox.constants as c
 
 
-def load_wav(filename, sample_rate):
-	audio, sr = librosa.load(filename, sr=sample_rate, mono=True)
-	audio = audio.flatten()
-	return audio
+class FastFourierTransform(object):
+    buckets = None
 
+    def __init__(self):
+        self.build_buckets()
 
-def normalize_frames(m,epsilon=1e-12):
-	return np.array([(v - np.mean(v)) / max(np.std(v),epsilon) for v in m])
+    @classmethod
+    def normalize_frames(cls, m, epsilon=1e-12):
+        return np.array([(v - np.mean(v)) / max(np.std(v), epsilon) for v in m])
 
+    @classmethod
+    # https://github.com/christianvazquez7/ivector/blob/master/MSRIT/rm_dc_n_dither.m
+    def remove_dc_and_dither(cls, sin, sample_rate):
+        if sample_rate == 16e3:
+            alpha = 0.99
+        elif sample_rate == 8e3:
+            alpha = 0.999
+        else:
+            print("Sample rate must be 16kHz or 8kHz only")
+            exit(1)
+        sin = lfilter([1, -1], [1, -alpha], sin)
+        dither = np.random.random_sample(len(sin)) + np.random.random_sample(len(sin)) - 1
+        spow = np.std(dither)
+        sout = sin + 1e-6 * spow * dither
+        return sout
 
-# https://github.com/christianvazquez7/ivector/blob/master/MSRIT/rm_dc_n_dither.m
-def remove_dc_and_dither(sin, sample_rate):
-	if sample_rate == 16e3:
-		alpha = 0.99
-	elif sample_rate == 8e3:
-		alpha = 0.999
-	else:
-		print("Sample rate must be 16kHz or 8kHz only")
-		exit(1)
-	sin = lfilter([1,-1], [1,-alpha], sin)
-	dither = np.random.random_sample(len(sin)) + np.random.random_sample(len(sin)) - 1
-	spow = np.std(dither)
-	sout = sin + 1e-6 * spow * dither
-	return sout
+    @classmethod
+    def get_fft_spectrum(cls, signal, buckets=None):
+        if buckets is None:
+            if FastFourierTransform.buckets is None:
+                FastFourierTransform.buckets = FastFourierTransform.build_buckets()
+                buckets = FastFourierTransform.buckets
+            else:
+                buckets = FastFourierTransform.buckets
 
+        # get FFT spectrum
+        signal = cls.remove_dc_and_dither(signal, c.SAMPLE_RATE)
+        signal = sigproc.preemphasis(signal, coeff=c.PREEMPHASIS_ALPHA)
+        frames = sigproc.framesig(signal, frame_len=c.FRAME_LEN * c.SAMPLE_RATE,
+                                  frame_step=c.FRAME_STEP * c.SAMPLE_RATE, winfunc=np.hamming)
+        fft = abs(np.fft.fft(frames, n=c.NUM_FFT))
+        fft_norm = cls.normalize_frames(fft.T)
 
-def get_fft_spectrum(filename, buckets):
-	signal = load_wav(filename,c.SAMPLE_RATE)
-	signal *= 2**15
+        # truncate to max bucket sizes
+        rsize = max(k for k in buckets if k <= fft_norm.shape[1])
+        rstart = int((fft_norm.shape[1] - rsize) / 2)
+        out = fft_norm[:, rstart:rstart + rsize]
 
-	# get FFT spectrum
-	signal = remove_dc_and_dither(signal, c.SAMPLE_RATE)
-	signal = sigproc.preemphasis(signal, coeff=c.PREEMPHASIS_ALPHA)
-	frames = sigproc.framesig(signal, frame_len=c.FRAME_LEN*c.SAMPLE_RATE, frame_step=c.FRAME_STEP*c.SAMPLE_RATE, winfunc=np.hamming)
-	fft = abs(np.fft.fft(frames,n=c.NUM_FFT))
-	fft_norm = normalize_frames(fft.T)
+        return out
 
-	# truncate to max bucket sizes
-	rsize = max(k for k in buckets if k <= fft_norm.shape[1])
-	rstart = int((fft_norm.shape[1]-rsize)/2)
-	out = fft_norm[:,rstart:rstart+rsize]
-
-	return out
-
-
-def build_buckets(max_sec = c.MAX_SEC, step_sec=c.BUCKET_STEP, frame_step=c.FRAME_STEP):
-	buckets = {}
-	frames_per_sec = int(1/frame_step)
-	end_frame = int(max_sec*frames_per_sec)
-	step_frame = int(step_sec*frames_per_sec)
-	for i in range(0, end_frame+1, step_frame):
-		s = i
-		s = np.floor((s-7+2)/2) + 1  # conv1
-		s = np.floor((s-3)/2) + 1  # mpool1
-		s = np.floor((s-5+2)/2) + 1  # conv2
-		s = np.floor((s-3)/2) + 1  # mpool2
-		s = np.floor((s-3+2)/1) + 1  # conv3
-		s = np.floor((s-3+2)/1) + 1  # conv4
-		s = np.floor((s-3+2)/1) + 1  # conv5
-		s = np.floor((s-3)/2) + 1  # mpool5
-		s = np.floor((s-1)/1) + 1  # fc6
-		if s > 0:
-			buckets[i] = int(s)
-	return buckets
+    @classmethod
+    def build_buckets(cls, max_sec=c.MAX_SEC, step_sec=c.BUCKET_STEP, frame_step=c.FRAME_STEP):
+        FastFourierTransform.buckets = {}
+        frames_per_sec = int(1 / frame_step)
+        end_frame = int(max_sec * frames_per_sec)
+        step_frame = int(step_sec * frames_per_sec)
+        for i in range(0, end_frame + 1, step_frame):
+            s = i
+            s = np.floor((s - 7 + 2) / 2) + 1  # conv1
+            s = np.floor((s - 3) / 2) + 1  # mpool1
+            s = np.floor((s - 5 + 2) / 2) + 1  # conv2
+            s = np.floor((s - 3) / 2) + 1  # mpool2
+            s = np.floor((s - 3 + 2) / 1) + 1  # conv3
+            s = np.floor((s - 3 + 2) / 1) + 1  # conv4
+            s = np.floor((s - 3 + 2) / 1) + 1  # conv5
+            s = np.floor((s - 3) / 2) + 1  # mpool5
+            s = np.floor((s - 1) / 1) + 1  # fc6
+            if s > 0:
+                FastFourierTransform.buckets[i] = int(s)
+        return FastFourierTransform.buckets
