f6b5e1d3a501e26aa1a9232761821350bb9c448d
Rattaphon Hokking <ratthapon.h@gmail.com>
2020-06-16 13:18:38 +0700

    Refactored VoxCeleb2Dataset, repeatable generator, refactored simple_test.py, cross template matching with generators

--- data/dataset/VoxCeleb2Dataset.py
+++ data/dataset/VoxCeleb2Dataset.py
@@ -1,28 +1,28 @@
 import re
 from abc import ABC
 
-from pydub import AudioSegment
 import tensorflow as tf
+from pydub import AudioSegment
 
 from feature.feature_extraction import FastFourierTransform
 
 
 class VoxCeleb2Dataset(tf.data.Dataset, ABC):
 
-    def __new__(cls, DATA_GLOB, batch: int = 16):
-        cls.filelist = tf.data.Dataset.list_files(DATA_GLOB)  # .as_numpy_iterator()
-
+    def __new__(cls, DATA_GLOB, batch: int = 16, repeat: int = 1):
         return tf.data.Dataset.from_generator(
             cls._m4a_generator,
             output_types=(tf.dtypes.int64, tf.dtypes.int16),
             output_shapes=(tf.TensorShape([1, None, None, 1]), tf.TensorShape([])),
-            args=(batch,),
+            args=(DATA_GLOB, batch,),
 
-        ).as_numpy_iterator()
+        ).repeat(repeat).as_numpy_iterator()
 
     @classmethod
-    def _m4a_generator(cls, batch: int = 1):
-        for encoded_filename in cls.filelist.take(batch).as_numpy_iterator():
+    def _m4a_generator(cls, DATA_GLOB, batch: int = 1):
+        filelist = tf.data.Dataset.list_files(DATA_GLOB, shuffle=False)
+
+        for encoded_filename in filelist.repeat().take(batch).as_numpy_iterator():
             filename = encoded_filename.decode('UTF-8')
 
             signal = AudioSegment.from_file(filename) \
@@ -33,3 +33,17 @@ class VoxCeleb2Dataset(tf.data.Dataset, ABC):
             id = int(re.search(r"id(\d+)", filename).group(1))
 
             yield feature.reshape(1, *feature.shape, 1), id
+
+    @classmethod
+    def meta_generator(cls, DATA_GLOB, batch: int = 1):
+        filelist = tf.data.Dataset.list_files(DATA_GLOB, shuffle=False).as_numpy_iterator()
+        meta_list = []
+        for encoded_filename in filelist:
+            filename = encoded_filename.decode('UTF-8')
+
+            id = re.search(r"(id\d+)", filename).group(1)
+            base_filename = re.search(r"id\d+\/(\w+.\w+.\w+)", filename).group(1)
+
+            meta_list += [(id, base_filename)]
+
+        return iter(meta_list)
--- test/simple_test.py
+++ test/simple_test.py
@@ -1,12 +1,23 @@
+import itertools
 import os
 
+import pandas
 import tensorflow as tf
 import numpy as np
+from pandas import DataFrame
+from tqdm import tqdm
 
 from config import constants as config
 import vggvox.model
 from data.dataset.VoxCeleb2Dataset import VoxCeleb2Dataset
-from model.VGGVox import VGGVox
+
+np.set_printoptions(precision=4)
+pandas.set_option("display.precision", 4)
+
+## disable to speed up, enable for as_numpy_iterator() execution
+## TODO refactor as_numpy_iterator to TF2 with disable eager execution
+# tf.compat.v1.disable_eager_execution()
+
 
 
 def average_cosine_score(
@@ -17,7 +28,17 @@ def average_cosine_score(
     Compute a score based on 3 enroll sample and use mean-reduced cosine distance
     score = 1 - MSE
     '''
-    return 1 - tf.math.reduce_sum(tf.math.squared_difference(enrolled, test), axis=axis)
+    scorer = tf.keras.metrics.CosineSimilarity(axis=0)
+    _ = scorer.update_state(enrolled, test)
+    return scorer.result().numpy()
+
+    # # faster cosine sim using matrix ops
+    # normalize_a = tf.nn.l2_normalize(enrolled, 1)
+    # normalize_b = tf.nn.l2_normalize(test, 1)
+    # similarity = tf.matmul(normalize_a, normalize_b, transpose_b=True)
+    # return similarity
+
+    # return 1 - tf.math.reduce_sum(tf.math.squared_difference(enrolled, test), axis=axis)
 
 
 model = vggvox.model.vggvox_model()
@@ -29,13 +50,65 @@ if __name__ == "__main__":
     REF_GLOB = REF_DIR + '*/*/*.m4a'
     EVAL_DIR = f"{HOME}/dataset/VoxCeleb2_simple3/eval/"
     EVAL_GLOB = EVAL_DIR + '*/*/*.m4a'
-    # TODO flatten files in datadir from '*/*/*.m4a' to '*/*.m4a'
-    n = 16
-    v = VoxCeleb2Dataset(DATA_GLOB=REF_GLOB, batch=n)
-    v2 = VoxCeleb2Dataset(DATA_GLOB=EVAL_GLOB, batch=n)
-    embs1 = model.predict_generator(v, steps=n)
-    embs2 = model.predict_generator(v2, steps=n)
-    # print(embs1, embs2, embs1.shape)
-    scores = average_cosine_score(embs1, embs2)
-    print("score", scores)
-    # print("score", np.squeeze(scores))
+
+    results = DataFrame(columns=["ref_id", "eval_id", "score", "ref_filename", "eval_filename"])
+    len_ref = len(list(VoxCeleb2Dataset.meta_generator(REF_GLOB)))
+    len_eval = len(list(VoxCeleb2Dataset.meta_generator(EVAL_GLOB)))
+    total_matches = len_ref * len_eval
+    print(f"len_ref: {len_ref}, len_eval: {len_eval}, total matches: {total_matches}")
+
+    batch_size = 4
+    n_batch = 1  # len(dataset) / batch_size
+    v = VoxCeleb2Dataset(DATA_GLOB=REF_GLOB, batch=len_ref)
+    v2 = VoxCeleb2Dataset(DATA_GLOB=EVAL_GLOB, batch=len_eval)
+
+    ref_metagen = VoxCeleb2Dataset.meta_generator(REF_GLOB)
+    eval_metagen = VoxCeleb2Dataset.meta_generator(EVAL_GLOB)
+
+    with tqdm(total=total_matches) as progress_bar:
+        # prefetch
+        ref_data, _ = v.next()
+        eval_data, _ = v2.next()
+        ref_id, ref_fname = ref_metagen.__next__()
+        eval_id, eval_fname = eval_metagen.__next__()
+        progress_bar.update(1)
+
+        for ref_idx in range(0, len_ref):
+            ref_template = np.squeeze(model.predict(ref_data, steps=1))
+
+            for eval_idx in range(0, len_eval):
+                eval_template = np.squeeze(model.predict(eval_data, steps=1))
+
+                result = DataFrame({
+                    "ref_id": [ref_id],
+                    "eval_id": [eval_id],
+                    "score": [average_cosine_score(ref_template, eval_template)],
+                    "ref_filename": [ref_fname],
+                    "eval_filename": [eval_fname]
+                })
+                results = results.append(result)
+                progress_bar.update(1)
+
+                try:
+                    eval_data, _ = v2.next()
+                    eval_id, eval_fname = eval_metagen.__next__()
+                except StopIteration:
+                    # TODO reset eval_metagen, v2
+                    # TODO prefetch sample
+                    v2 = VoxCeleb2Dataset(DATA_GLOB=EVAL_GLOB, batch=len_eval)
+                    eval_metagen = VoxCeleb2Dataset.meta_generator(EVAL_GLOB)
+                    eval_data, _ = v2.next()
+                    eval_id, eval_fname = eval_metagen.__next__()
+
+                    results.to_csv("spkr_ver_scores.csv")
+                    break
+
+            try:
+                ref_data, _ = v.next()
+                ref_id, ref_fname = ref_metagen.__next__()
+            except StopIteration:
+                print("Finished")
+                break
+
+        results.to_csv("spkr_ver_scores.csv")
+        print(results)
